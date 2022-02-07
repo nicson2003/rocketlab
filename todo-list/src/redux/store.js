1da54daf269923e30b849c5e92b0536f56100d5c
nicsondelatorre <nicson@mandnweb.com>
2022-02-07 18:58:29 +0800

    remove redux dev tools

--- todo-list/src/redux/store.js
+++ todo-list/src/redux/store.js
@@ -13,8 +13,8 @@ import rootSaga from './sagas/index';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = compose(
-  applyMiddleware(sagaMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(sagaMiddleware)
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )(createStore)(rootReducer);
 
 sagaMiddleware.run(rootSaga);
